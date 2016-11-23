e5253593662183571c85ebfc6f8222db06a6dc87
making <makingx@gmail.com>
2016-11-24 02:50:34 +0900

    first commit

---
+++ src/main/java/com/example/EcWebApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class EcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcWebApplication.class, args);
+	}
+}
